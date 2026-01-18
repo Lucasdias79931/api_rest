3d0b1dd5db1b5f484d4f8a026273504da3fc03fa
lucasdias79931 <lucasdias26@hotmail.com.br>
2026-01-18 14:49:35 -0300

    designing repository

--- src/repositories/repository.py
+++ src/repositories/repository.py
@@ -1,38 +1,43 @@
 from abc import ABC, abstractmethod
+from typing import Generic, TypeVar, Iterable
+
+T = TypeVar("T")
+ID = TypeVar("ID")
 
 
-class Repository(ABC):
-    
-    @abstractmethod
-    def save(entity)->None:
-        """
-            This method must be implemented to persist entity in database        
-        """
-        pass
+class Repository(ABC, Generic[T, ID]):
 
     @abstractmethod
-    def get_by_id(entity_id):
+    def save(self, entity: T) -> None:
         """
-            This method must be implemented to load entity by id from database      
+        This method must be implemented to persist an entity in the database.
         """
+        ...
 
-    @staticmethod
-    @property
     @abstractmethod
-    def get_all():
-         """
-            This method must be implemented to load all entities from database      
+    def get_by_id(self, entity_id: ID) -> T | None:
         """
+        This method must be implemented to load an entity by id from the database.
+        """
+        ...
+
     @abstractmethod
-    def delete_or_mark_inactive(self, entity_id):
+    def get_all(self) -> Iterable[T]:
         """
-            This method must be implemented to delete or mark a entity as inactive in database   
+        This method must be implemented to load all entities from the database.
         """
-    
+        ...
+
     @abstractmethod
-    @staticmethod
-    def update(entity):
+    def update(self, entity: T) -> None:
         """
-            This method must be implemented to update entity in database  
+        This method must be implemented to update an entity in the database.
         """
-    
+        ...
+
+    @abstractmethod
+    def delete_or_mark_inactive(self, entity_id: ID) -> None:
+        """
+        This method must be implemented to delete or mark an entity as inactive in the database.
+        """
+        ...
